9811337b496afc5abbc806e468d02cc1bc000d65
VuTuanCuong <114804308+Vucuongtuan@users.noreply.github.com>
2024-05-10 17:04:16 +0700

    ádasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
-import { HomePage, DetailMovie } from './pages';
 import { Header, Footer } from './components';
 import GlobalStyle from './components/GlobalStyle';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ViewMovie from './pages/viewMovie';
-import ListMovie from './pages/ListMovie';
-import TheLoai from './pages/ListMovie/theloai';
-import CountryPage from './pages/ListMovie/quocgia';
+// import { HomePage, DetailMovie } from './pages';
+// import ViewMovie from './pages/viewMovie';
+// import ListMovie from './pages/ListMovie';
+// import TheLoai from './pages/ListMovie/theloai';
+// import CountryPage from './pages/ListMovie/quocgia';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, useEffect, useState } from 'react';
 import axios from 'axios';
+import LoadingElement from './components/LoadingElement';
+const HomePage = lazy(() => import('./pages/Home/HomePage'));
+const DetailMovie = lazy(() => import('./pages/Detail/DetailMovie'));
+const ViewMovie = lazy(() => import('./pages/viewMovie'));
+const ListMovie = lazy(() => import('./pages/ListMovie'));
+const TheLoai = lazy(() => import('./pages/ListMovie/theloai'));
+const CountryPage = lazy(() => import('./pages/ListMovie/quocgia'));
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -45,17 +52,19 @@ function App() {
   return (
     <GlobalStyle>
       <QueryClientProvider client={queryClient}>
-        {!isLoginPage && <Header />}
-        <Routes>
-          <Route path='/' element={<HomePage />} />
+        <Suspense fallback={<LoadingElement />}>
+          {!isLoginPage && <Header />}
+          <Routes>
+            <Route path='/' element={<HomePage />} />
 
-          <Route path='/details/:slug' element={<DetailMovie />} />
-          <Route path='/movie/:name/:slug' element={<ViewMovie />} />
-          <Route path='/danh-sach/:slug' element={<ListMovie />} />
-          <Route path='/the-loai/:slug' element={<TheLoai />} />
-          <Route path='/quoc-gia/:nation' element={<CountryPage />} />
-        </Routes>
-        {!isLoginPage && <Footer />}{' '}
+            <Route path='/details/:slug' element={<DetailMovie />} />
+            <Route path='/movie/:name/:slug' element={<ViewMovie />} />
+            <Route path='/danh-sach/:slug' element={<ListMovie />} />
+            <Route path='/the-loai/:slug' element={<TheLoai />} />
+            <Route path='/quoc-gia/:nation' element={<CountryPage />} />
+          </Routes>
+          {!isLoginPage && <Footer />}{' '}
+        </Suspense>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </GlobalStyle>
